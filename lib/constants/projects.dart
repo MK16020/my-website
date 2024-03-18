a170e7181edc902d1305702dbf108066441a0196
MK16020 <61877101+MK16020@users.noreply.github.com>
2024-03-18 12:41:39 +0300

    Project section Update
    This update will add images to projects model and edit project section to show these changes

--- lib/constants/projects.dart
+++ lib/constants/projects.dart
@@ -1,22 +1,61 @@
 const List<Map> projectInfo = [
   {
-    'img': 'assets/images/Demo_project.jpg',
-    'title': 'Vepaar frontend Project',
+    'images': [
+      'assets/images/Vepaar/add_contacts.png',
+      'assets/images/Vepaar/tickets.png',
+      'assets/images/Vepaar/add_reminder.png',
+      'assets/images/Vepaar/add_reminder2.png',
+      'assets/images/Vepaar/contacts.png',
+      'assets/images/Vepaar/dashboard.png',
+      'assets/images/Vepaar/login.png',
+      'assets/images/Vepaar/register.png',
+      'assets/images/Vepaar/reminder.png',
+    ],
+    'title': 'Vepaar frontend',
     'description': 'This project is a Vepaar frontend mockup. Vepaar is a CRM that helps   ',
+    'GitHub': 'https://github.com/MK16020/project_4',
   },
   {
-    'img': 'assets/images/Demo_project.jpg',
+    'images': [
+      'assets/images/Blog/about_app.png',
+      'assets/images/Blog/add_blog.png',
+      'assets/images/Blog/blog_page.png',
+      'assets/images/Blog/category_page.png',
+      'assets/images/Blog/Edit_blog.png',
+      'assets/images/Blog/Favorite_page.png',
+      'assets/images/Blog/home_page.png',
+      'assets/images/Blog/home_page2.png',
+      'assets/images/Blog/login.png',
+      'assets/images/Blog/other_user.png',
+      'assets/images/Blog/settings_page.png',
+      'assets/images/Blog/user_profile.png',
+    ],
     'title': 'Blog Project',
-    'description': ' An online journal or informational website that offers regularly updated content (blog post) about a topic. It presents information in reverse chronological order and it\'s written in an informal or conversational style.',
+    'description':
+        ' An online journal or informational website that offers regularly updated content (blog post) about a topic. It presents information in reverse chronological order and it\'s written in an informal or conversational style.',
+    'GitHub': 'https://github.com/MK16020/project_2',
   },
   {
-    'img': 'assets/images/Demo_project.jpg',
+    'images': [
+      'assets/images/E-number/all_elements.png',
+      'assets/images/E-number/e-number_sample.png',
+      'assets/images/E-number/Search_resualt2.png',
+      'assets/images/E-number/Search_result.png',
+    ],
     'title': 'E number Project',
-    'description': 'E numbers are codes for food additives and are usually found on food labels throughout the European Union. This app will detect the E number if canned food and break it down to you. whether the E number is a color or a Preservatives etc, the app will give you a full info about it.',
+    'description':
+        'E numbers are codes for food additives and are usually found on food labels throughout the European Union. This app will detect the E number if canned food and break it down to you. whether the E number is a color or a Preservatives etc, the app will give you a full info about it.',
+    'GitHub': 'https://github.com/ZainabAdel371/Enumber',
   },
   {
-    'img': 'assets/images/Demo_project.jpg',
+    'images': [
+      'assets/images/Demo_project.jpg',
+      'assets/images/Clerkish/Code.png',
+      'assets/images/Clerkish/code2.png',
+    ],
     'title': 'Clerkish Project',
-    'description': ' This is the backend of a enterprise resource planing application. It helps you organizations use to manage day-to-day business activities such as accounting, procurement, project management, risk management and compliance, and supply chain operations. ',
+    'description':
+        ' This is the backend of a enterprise resource planing application. It helps you organizations use to manage day-to-day business activities such as accounting, procurement, project management, risk management and compliance, and supply chain operations. ',
+    'GitHub': 'https://github.com/MK16020/Clerkish',
   },
 ];
--- lib/widgets/project_desktop.dart
+++ lib/widgets/project_desktop.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:my_website/styles/custom_button.dart';
-import 'package:my_website/styles/text_header.dart';
 
 import '../constants/colors.dart';
+import '../constants/projects.dart';
+import '../styles/custom_button.dart';
 import '../styles/regular_text.dart';
+import '../styles/text_header.dart';
 import '../styles/text_header_2.dart';
 
 class ProjectDesktop extends StatelessWidget {
@@ -19,49 +23,75 @@ class ProjectDesktop extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 30),
       color: CustomColor.whiteSecondary,
+      width: screenWidth,
       child: Column(
         children: [
           TextHeader(data: 'My Works', fontSize: (screenWidth / 20 < 30 ? 30 : screenWidth / 20)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const SizedBox(height: 30),
-              Container(
-                color: CustomColor.bgLight1,
-                width: screenWidth * 0.45,
-                height: 500,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/Demo_project.jpg'),
-                    ),
-                  ),
-                  margin: const EdgeInsets.all(10),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+          for (var project in projectInfo)
+            Container(
+              margin: const EdgeInsets.only(bottom: 100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextHeader2(
-                    data: 'Project name',
-                    fontSize: (screenWidth / 25 < 20 ? 20 : screenWidth / 25),
-                  ),
-                  SizedBox(
-                    width: screenWidth * 0.3,
-                    child: const RegularText(
-                      data:
-                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                  const SizedBox(height: 30),
+                  Container(
+                    color: CustomColor.bgLight1,
+                    width: screenWidth * 0.45,
+                    height: 500,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      child: CarouselSlider.builder(
+                        itemCount: project['images'].length,
+                        itemBuilder: ((context, index, realIndex) {
+                          return buildImage(project['images'][index]);
+                        }),
+                        options: CarouselOptions(
+                          height: 450,
+                          viewportFraction: 1,
+                          autoPlay: true,
+                          autoPlayInterval: Duration(seconds: Random().nextInt(4) + 2),
+                        ),
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 30),
-                  CustomButton(title: 'Learn More', onPressed: () {}, color: CustomColor.btnPrimary),
-                  const SizedBox(height: 30),
+                  const SizedBox(width: 30),
+                  SizedBox(
+                    width: screenWidth * 0.35,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        TextHeader2(
+                          data: project['title'],
+                          fontSize: (screenWidth / 25 < 20 ? 20 : screenWidth / 25),
+                        ),
+                        SizedBox(
+                          width: screenWidth * 0.3,
+                          child: RegularText(
+                            data: project['description'],
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        CustomButton(title: 'View it in GitHub', onPressed: () {}, color: CustomColor.btnPrimary),
+                        const SizedBox(height: 30),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            ],
-          ),
+            ),
+          const SizedBox(height: 60),
         ],
       ),
     );
   }
+
+  Widget buildImage(String project) => Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(project),
+          ),
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+      );
 }
